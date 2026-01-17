1edfea917875c6a4af317eea9489da724298e23a
hero <icraft2170@gmail.com>
2026-01-17 16:44:37 +0900

    Step04 : 첫 번째 LLM 호출

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,32 @@
 package theunderdog.ai
 
+import ai.koog.prompt.dsl.prompt
 import ai.koog.prompt.executor.clients.anthropic.AnthropicLLMClient
+import ai.koog.prompt.executor.clients.anthropic.AnthropicModels
 import ai.koog.prompt.executor.llms.SingleLLMPromptExecutor
 
-fun main() {
+suspend fun main() {
     val apiKey = System.getenv("ANTHROPIC_API_KEY")
         ?: error("API KEY가 설정되지 않았습니다.")
 
     val client = AnthropicLLMClient(apiKey)
     val executor = SingleLLMPromptExecutor(client)
-    println("Prompt Executor 생성 완료")
+
+    // 사용자 입력 받기
+    print("User: ")
+    val userPrompt = readln()
+
+    // 프롬프트 구성
+    val prompt = prompt(id = "hello-koog") {
+        system("당신은 코딩 에이전트 입니다.")
+        user(userPrompt)
+    }
+
+    //LLM에 요청 보내고 응답 받기
+    val response = executor.execute(
+        prompt = prompt,
+        model = AnthropicModels.Opus_4_5
+    )
+
+    println("Assistant: ${response.first().content}")
 }
